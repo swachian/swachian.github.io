8668cbe34d19d45b4761c8d5037cc2ffe5e44811
zhangyu34@gmail.com <zhangyu34@gmail.com>
2025-05-02 20:26:01 +0800

    only index

--- ping.rb
+++
@@ -1,5 +0,0 @@
-domains = ['jp1', 'jp2', 'jp3', 'us1', 'us2', 'us3', 'sg1', 'sg2', 'tw1']
-
-domains.each do |domain|
-  puts `ping #{domain}.seeplease.com`
-end
--- source/images/resize.rb
+++
@@ -1,7 +0,0 @@
-p ARGV
-
-images = `ls #{ARGV[0]}*.jpg`.split(/\s+/)
-images.each do |img|
-   puts img
-   `convert -resize 20%x20% #{img} #{img}`
-end
